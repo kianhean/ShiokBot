92b394e75fd4dbaf713d7e2d07a50bdc7415b033
Kian Hean <kianhean@gmail.com>
2017-03-11 11:06:55 +0800

    added 4d results

--- bot/core.py
+++ bot/core.py
@@ -9,6 +9,8 @@ import logging
 import json
 import requests
 from telegram.ext import Updater, CommandHandler
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 
 # Enable logging
@@ -31,6 +33,53 @@ def connnect_gov_api(url_string):
     request = requests.get(url_string, headers=headers)
 
     return request
+
+
+def FourD():
+    """4D Methods!"""
+
+    # Connect to Source
+    url = 'https://www.gidapp.com/lottery/singapore/4d'
+    data = urlopen(url)
+    soup = BeautifulSoup(data, 'html.parser')
+
+    # Find latest Result
+    result = soup.findAll('time')
+    latest_result_date = result[6].get_text()
+
+    result = soup.find_all('span')
+
+    # TOP PRIZES
+    first = result[8].get_text()
+    second = result[9].get_text()
+    third = result[10].get_text()
+
+    top_text = "1st - " + first + "\n2nd - " + second + "\n3rd - " + third + "\n\n"
+
+    # SPECIAL PRIZE
+    special_prize = "Special/Starter Prizes\n\n"
+    for i in range(10, 21):
+        special_prize +=  result[i].get_text() + "  "
+
+    # CONSOLATION PRIZE
+    consol_prize = "\n\nConsolation Prizes\n\n"
+    for i in range(21, 31):
+        consol_prize +=  result[i].get_text() + "  "
+
+    # Create Reply
+    chat_reply = "Latest Draw Results on " + latest_result_date + " \n\n"
+    chat_reply += top_text
+    chat_reply += special_prize
+    chat_reply += consol_prize
+    chat_reply += "\n\n No need check la sure never win!"
+
+    return chat_reply
+
+
+def fourdresults(bot, update):
+    """ Send results from 4D """
+    final_string = FourD()
+    bot.sendMessage(update.message.chat_id, text=final_string, parse_mode='HTML')
 
 
 def traffic(bot, update, args):
@@ -155,6 +204,7 @@ def main():
     dispatch.add_handler(CommandHandler("help", help))
     dispatch.add_handler(CommandHandler("psi", psi3hour))
     dispatch.add_handler(CommandHandler("weather", weathernow))
+    dispatch.add_handler(CommandHandler("4d", fourdresults))
     dispatch.add_handler(CommandHandler("traffic", traffic, pass_args=True))
 
     # log all errors
