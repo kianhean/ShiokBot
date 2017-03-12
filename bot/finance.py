64a93fe36207b61862a08c173bf404c4313cffbe
kianhean <kianhean@gmail.com>
2017-03-12 12:54:58 +0800

    fix sorting

--- bot/finance.py
+++ bot/finance.py
@@ -30,7 +30,7 @@ def get_fx():
 
     text_final = '<b>Latest SGD End of Day Rates ' + result_today['end_of_day'] + '</b>\n\n'
 
-    for key in list_curr:
-        text_final += "1 SGD = " + str(list_curr[key]) + " " + key + "\n"
+    for key in sorted(list_curr.keys()):
+        text_final += key + " " + str(list_curr[key]) + " = 1 SGD \n"
     return text_final
 
